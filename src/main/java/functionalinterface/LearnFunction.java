96ef13952cd143ce52a951e3477a7e5f7a19f92b
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-28 20:46:57 +0530

    changes to fix naming convention

--- src/main/java/functionalinterface/LearnFunction.java
+++ src/main/java/functionalinterface/LearnFunction.java
@@ -8,21 +8,23 @@ public class LearnFunction {
         Integer val = increment(0);
         System.out.println(val);    //1
 
-        val = IncrementByOne.andThen(MultiplyByTwo).apply(val);
+        //Function
+        val = incrementByOne.andThen(multiplyByTwo).apply(val);
         System.out.println(val);    //4
 
-        val = IncrementByOne
-                .andThen(MultiplyByTwo
-                        .andThen(MultiplyByTwo)
-                        .andThen(IncrementByOne)
-                        .andThen(MultiplyByTwo))
+        val = incrementByOne
+                .andThen(multiplyByTwo
+                        .andThen(multiplyByTwo)
+                        .andThen(incrementByOne)
+                        .andThen(multiplyByTwo))
                 .apply(val);
         System.out.println(val);    //42
     }
 
     static int increment(Integer a){return ++a;}
 
-    static Function<Integer, Integer> IncrementByOne = num -> ++num;
-    static Function<Integer, Integer> MultiplyByTwo = num -> 2*num;
+    static Function<Integer, Integer> incrementByOne = num -> ++num;
+    static Function<Integer, Integer> multiplyByTwo = num -> 2*num;
+
 
 }
