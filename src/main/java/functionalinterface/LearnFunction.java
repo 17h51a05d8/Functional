5098e42081da68c9df6065113bd9151ae7c2b904
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-28 20:18:09 +0530

    Function chaining using 'andThen'

--- src/main/java/functionalinterface/LearnFunction.java
+++ src/main/java/functionalinterface/LearnFunction.java
@@ -6,10 +6,18 @@ public class LearnFunction {
     public static void main(String[] args) {
 
         Integer val = increment(0);
-        System.out.println(val);
+        System.out.println(val);    //1
 
         val = IncrementByOne.andThen(MultiplyByTwo).apply(val);
-        System.out.println(val);
+        System.out.println(val);    //4
+
+        val = IncrementByOne
+                .andThen(MultiplyByTwo
+                        .andThen(MultiplyByTwo)
+                        .andThen(IncrementByOne)
+                        .andThen(MultiplyByTwo))
+                .apply(val);
+        System.out.println(val);    //42
     }
 
     static int increment(Integer a){return ++a;}
