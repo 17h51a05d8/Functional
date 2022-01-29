96cecc6eea06d314dc9e80c8127cbdd069517d7a
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-29 10:45:31 +0530

    BiConsumer Introduction

--- src/main/java/functionalinterface/LearnConsumer.java
+++ src/main/java/functionalinterface/LearnConsumer.java
@@ -1,13 +1,14 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class LearnConsumer {
 
     static class Customer{
 
-        private String name;
-        private String phoneNumber;
+        private final String name;
+        private final String phoneNumber;
 
         public Customer(String name, String phoneNumber) {
             this.name = name;
@@ -24,14 +25,23 @@ public class LearnConsumer {
             customer -> System.out.println("Hello" + customer.name +
                     ". Thanks for registering phone number "+ customer.phoneNumber);
 
+    static BiConsumer<Customer, Customer> customerSong = (cust1, cust2) -> {
+        System.out.println("Na mai samjha, Na mai jaana, tumne jo bhi mujhse kaha hai "+ cust1.name);
+        System.out.println("O " + cust2.name +" O " + cust2.name +" O "+cust2.name+" Oh Oo");
+    };
+
     public static void main(String[] args) {
         Customer maria = new Customer("Maria", "4399428490");
+        Customer senorita = new Customer("Senorita", "4399428490");
 
         //java Function
         greetCustomer(maria);
 
         //Consumer Functional Interface
-        greetCustomerConsumer.accept(maria);
+        greetCustomerConsumer.accept(senorita);
+
+        //BiConsumer Functional Interface
+        customerSong.accept(senorita, maria);
     }
 
 }
